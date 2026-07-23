e19f94ddb010141ae4c1cc4f7d49ab2a62a7074d
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2026-07-22 18:08:55 -0700

    suggested paths config

--- bluetooth-tunnels-web/vite.config.ts
+++ bluetooth-tunnels-web/vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -11,7 +11,19 @@ export default defineConfig({
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
-			adapter: adapter()
+			adapter: adapter({
+				// default options are shown. On some platforms
+				// these options are set automatically — see below
+				pages: 'build',
+				assets: 'build',
+				fallback: undefined,
+				precompress: false,
+				strict: true
+			}),
+
+			paths: {
+				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			}
 		})
 	]
 });
